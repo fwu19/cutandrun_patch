d4782cc3f8864e309352c0de8f62a5a297dc635c
Feinan Wu <fwu@fredhutch.org>
2024-10-23 21:12:05 -0700

    relocate files

--- bin/get_fq_path.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "seq.id,fastq_1,fastq_2" >fq.csv
-for fqdir in $@; do
-	paste -d "," <(find $fqdir/Unaligned/ | egrep "_S[0-9]+_(L[0-9]+_)?R1_.*fastq.gz" | sort | sed "s/.*\///g; s/_S[0-9]\+.*_R1_.*//g" ) <(find $fqdir/Unaligned/ | egrep "_S[0-9]+_(L[0-9]+_)?R1_.*fastq.gz" | sort ) <(find $fqdir/Unaligned/ | egrep "_S[0-9]+_(L[0-9]+_)?R2_.*fastq.gz" |sort )
-done >>fq.csv
-
